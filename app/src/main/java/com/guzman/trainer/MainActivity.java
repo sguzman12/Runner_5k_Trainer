728e82fb210fdef7d974fd74c881c46fcf0f383b
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2020-05-30 12:26:52 -0400

    initial commit

---
+++ app/src/main/java/com/guzman/trainer/MainActivity.java
@@ -0,0 +1,19 @@
+package com.guzman.trainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStart(View view){
+
+    }
+}
