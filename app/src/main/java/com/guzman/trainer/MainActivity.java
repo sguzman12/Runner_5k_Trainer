8e7b011c89764da91ec4868bc70724e8287b7f4b
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2020-06-02 15:24:50 -0400

    **MainActivity Added defensive programming to the MainActivity class. Checks that user has selected something. Continues and then grabs item selected and Casts the first char into an Integer. That value is returned and put in the intent calling the TrainingActivity class.
    **TrainingActivity
    Added a simple command inside the onCreate method which "gets" the intent value of the runintensity and saves to the EXTRA_INTENSITY variable. System.out added to test it is working.

--- app/src/main/java/com/guzman/trainer/MainActivity.java
+++ app/src/main/java/com/guzman/trainer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 //Sigfredo Guzman
 //Date 5-31-2020
 //Purpose: Android App allows the user to train incrementally towards the goal
@@ -18,8 +19,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onStart(View view){
-        Intent intent = new Intent(this, MapsActivity.class);
+    /**
+     * Called from button click. Adds the run intensity as an Extra Intent to
+     * the Training Activity.
+     * @param view
+     */
+    public void onStart(View view) {
+        int intensity = runIntensity();
+
+        Intent intent = new Intent(this, TrainingActivity.class);
+        intent.putExtra("EXTRA_INTENSITY", intensity);
         startActivity(intent);
+    }
+
+    /**
+     * Find the intensity of the run selected.
+     */
+    private int runIntensity() {
+        int intensity = 0;
+
+        Spinner run = (Spinner) findViewById(R.id.intensity);
+        if (run.getSelectedItemPosition() > 0) {
+            try {
+                String s = String.valueOf(run.getSelectedItem());
+
+                intensity = Integer.parseInt(String.valueOf(s.charAt(0)));
+
+                return intensity;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Else statement");
+        }
+        return intensity;
     }
 }
--- app/src/main/java/com/guzman/trainer/TrainingActivity.java
+++ app/src/main/java/com/guzman/trainer/TrainingActivity.java
@@ -2,14 +2,22 @@ package com.guzman.trainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class TrainingActivity extends AppCompatActivity {
+
+    public static final int EXTRA_INTENSITY = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
+
+        Intent intent = getIntent();
+        int i = intent.getIntExtra("EXTRA_INTENSITY", EXTRA_INTENSITY);
+
+        System.out.println(i);
     }
 
 
