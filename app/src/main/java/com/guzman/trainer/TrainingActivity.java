0a6b2df2aea10f480410835612f4692f4317c386
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2020-07-02 13:36:40 -0400

    Building Location Service and permissions
    Signed-off-by: Sigfredo Guzman <sigfredo.guzman@gmail.com>

--- app/src/main/java/com/guzman/trainer/TrainingActivity.java
+++ app/src/main/java/com/guzman/trainer/TrainingActivity.java
@@ -1,15 +1,21 @@
 package com.guzman.trainer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.Observer;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.guzman.model.TrainingModel;
+import com.guzman.viewmodel.GPS_Service;
 import com.guzman.viewmodel.TimerFactory_ViewModel;
 import com.guzman.viewmodel.Timer_ViewModel;
 
@@ -42,7 +48,7 @@ public class TrainingActivity extends AppCompatActivity
         mViewModel = new ViewModelProvider(this, factory).get(Timer_ViewModel.class);
 
         observeTimer();
-
+        startLocationService();
     }
 
     /**
@@ -50,9 +56,11 @@ public class TrainingActivity extends AppCompatActivity
      * to TextViews.
      */
 
-    private void observeTimer() {
+    private void observeTimer()
+    {
 
-        final Observer elapsedTimeObserver = new Observer<TrainingModel>() {
+        final Observer elapsedTimeObserver = new Observer<TrainingModel>()
+        {
 
 
             /**
@@ -62,7 +70,8 @@ public class TrainingActivity extends AppCompatActivity
              */
 
             @Override
-            public void onChanged(TrainingModel tm) {
+            public void onChanged(TrainingModel tm)
+            {
 
                 final TextView sectionCountdown = (TextView) findViewById(R.id.countdownTimer);
                 final TextView sectionActivity = (TextView) findViewById(R.id.runMessage);
@@ -80,6 +89,7 @@ public class TrainingActivity extends AppCompatActivity
 
                 if (sectionSeconds == -1) {
                     sectionCountdown.setText("Finished");
+                    //stopService();
                 }
 
             }
@@ -89,7 +99,58 @@ public class TrainingActivity extends AppCompatActivity
         mViewModel.getCurrent().observe(this, elapsedTimeObserver);
     }
 
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on {@link #requestPermissions(String[], int)}.
+     * <p>
+     * <strong>Note:</strong> It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     * </p>
+     *
+     * @param requestCode  The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *                     which is either {@link PackageManager#PERMISSION_GRANTED}
+     *                     or {@link PackageManager#PERMISSION_DENIED}. Never null.
+     * @see #requestPermissions(String[], int)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        switch (requestCode){
+            case 1:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startService();
+                } else {
+                    Toast.makeText(this, "Permissions Needed", Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+
+    private void startLocationService()
+    {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //Request Location
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                //Request Location Permission
+
+            }
+
+        } else {
+            //Start Location Service
+
+        }
+    }
+
+    private void startService(){
+        Intent intent = new Intent(TrainingActivity.this, GPS_Service.class);
+        startService(intent);
+    }
 
 }
 
