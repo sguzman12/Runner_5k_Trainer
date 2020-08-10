9300403b08f733797cce6e3201a05492e155aa6b
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2020-08-10 14:06:56 -0400

    Refactored GPS_Service
    Signed-off-by: Sigfredo Guzman <sigfredo.guzman@gmail.com>

--- app/src/main/java/com/guzman/viewmodel/GPS_Service.java
+++ app/src/main/java/com/guzman/viewmodel/GPS_Service.java
@@ -1,116 +1,121 @@
 package com.guzman.viewmodel;
 
-import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
+import android.nfc.Tag;
+import android.os.Bundle;
 import android.os.IBinder;
-import android.os.Looper;
+import android.os.Binder;
+import android.location.LocationListener;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.Criteria;
+import android.content.pm.PackageManager;
 import android.util.Log;
+import android.webkit.GeolocationPermissions;
 
-import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.MutableLiveData;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
+import java.util.ArrayList;
+import com.guzman.model.GeoLocationObjectModel;
 
 public class GPS_Service extends Service
 {
-    FusedLocationProviderClient fusedLocationProviderClient;
-    LocationCallback callback;
+    private GeoLocationObjectModel geo;
+    private final IBinder binder = new OdometerBinder();
+    private LocationListener listener;
+    private LocationManager locManager;
+    private static double distanceInMeters;
+    private static Location lastLocation = null;
+    public static final String PERMISSION_STRING
+            = android.Manifest.permission.ACCESS_FINE_LOCATION;
 
-    public GPS_Service()
+    public class OdometerBinder extends Binder
     {
+        GPS_Service getOdometer()
+        {
+            return GPS_Service.this;
+        }
     }
 
-    /**
-     * Called by the system when the service is first created.  Do not call this method directly.
-     */
     @Override
     public void onCreate()
     {
         super.onCreate();
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        callback = new LocationCallback()
+
+        listener = new LocationListener()
         {
             @Override
-            public void onLocationResult(LocationResult locationResult)
+            public void onLocationChanged(Location location)
             {
-                super.onLocationResult(locationResult);
-                Log.d("mylog", "Lat is: " + locationResult.getLastLocation().getLatitude() + ", "
-                        + "Lng is: " + locationResult.getLastLocation().getLongitude());
-                System.out.println("Lat is: " + locationResult.getLastLocation().getLatitude() + ", "
-                        + "Lng is: " + locationResult.getLastLocation().getLongitude());
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+
+                //Create Geolocation object
+                geo = new GeoLocationObjectModel(lastLocation.getLatitude(), lastLocation.getLongitude());
+
+            }
+
+            @Override
+            public void onProviderDisabled(String arg0)
+            {
+            }
+
+            @Override
+            public void onProviderEnabled(String arg0)
+            {
+            }
+
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle bundle)
+            {
             }
         };
-    }
 
-    /**
-     * Called by the system every time a client explicitly starts the service by calling
-     * {@link Context#startService}, providing the arguments it supplied and a
-     * unique integer token representing the start request.  Do not call this method directly.
-     *
-     * <p>For backwards compatibility, the default implementation calls
-     * {@link #onStart} and returns either {@link #START_STICKY}
-     * or {@link #START_STICKY_COMPATIBILITY}.
-     *
-     * <p class="caution">Note that the system calls this on your
-     * service's main thread.  A service's main thread is the same
-     * thread where UI operations take place for Activities running in the
-     * same process.  You should always avoid stalling the main
-     * thread's event loop.  When doing long-running operations,
-     * network calls, or heavy disk I/O, you should kick off a new
-     * thread, or use {@link AsyncTask}.</p>
-     *
-     * @param intent  The Intent supplied to {@link Context#startService},
-     *                as given.  This may be null if the service is being restarted after
-     *                its process has gone away, and it had previously returned anything
-     *                except {@link #START_STICKY_COMPATIBILITY}.
-     * @param flags   Additional data about this start request.
-     * @param startId A unique integer representing this specific request to
-     *                start.  Use with {@link #stopSelfResult(int)}.
-     * @return The return value indicates what semantics the system should
-     * use for the service's current started state.  It may be one of the
-     * constants associated with the {@link #START_CONTINUATION_MASK} bits.
-     * @see #stopSelfResult(int)
-     */
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
-        requestLocation();
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    private void requestLocation()
-    {
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING)
+                == PackageManager.PERMISSION_GRANTED) {
+            String provider = locManager.getBestProvider(new Criteria(), true);
+            if (provider != null) {
+                locManager.requestLocationUpdates(provider, 3000, 5, listener);
+            }
         }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, callback, Looper.myLooper());
     }
 
     @Override
     public IBinder onBind(Intent intent)
     {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-
-
+        return binder;
     }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (locManager != null && listener != null) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING)
+                    == PackageManager.PERMISSION_GRANTED) {
+                locManager.removeUpdates(listener);
+            }
+            locManager = null;
+            listener = null;
+        }
+    }
+
+    public double getDistance()
+    {
+        return this.distanceInMeters / 1609.344;
+    }
+
+    public GeoLocationObjectModel getCoordinates()
+    {
+        return geo;
+    }
+
+
 }
